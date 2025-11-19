650bbb087cb7d47b2156ab60f1e71034e687f970
Joel <joel2thewrld@gmail.com>
2025-11-19 06:40:16 +0100

    Revert "Newdashboard1"

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-// import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 // import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,7 +15,13 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 
-		adapter: adapter(),
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
 		prerender: {
 			entries: [
 				'/',
@@ -77,6 +83,7 @@ const config = {
 				'/home/publications/publicationstatusupdate/statusupdateform',
 				'/opposition',
 				'/opposition/paid',
+				'/home/file-withdrawal',
 				'/home/file-withdrawal/search',
 				'/home/file-withdrawal/withdrawalform',
 				'/home/file-withdrawal/file-withdrawal-result',
