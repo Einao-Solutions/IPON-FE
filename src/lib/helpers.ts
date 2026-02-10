5e0a78081f84752c1f62303fef62f89588df68df
joel <joel.eriba@einaosolutions.com>
2026-02-10 08:33:55 +0100

    fix: update application certificate handling logic and adjust baseURL configuration

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -6,8 +6,8 @@ import { loggedInUser } from "$lib/store";
 import { goto } from "$app/navigation";
 import type { A } from "vitest/dist/chunks/environment.LoooBwUu.js";
 
-// export const baseURL = "http://localhost:5044";
-export const baseURL = "https://backend.einaotest.com";
+export const baseURL = "http://localhost:5044";
+// export const baseURL = "https://backend.einaotest.com";
 // export const baseURL = "https://integration.iponigeria.com";
 export const localhost = "http://localhost:5044";
 
