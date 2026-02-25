f1e150073db5e3496c32b58808e0e87bc7f9ad7b
Joel <joel,eriba@einaosolutions.com>
2026-02-25 13:16:50 +0100

    fix: update user ID reference in ManualUpdate API call

--- src/routes/dataview/ApplicationsHistory.svelte
+++ src/routes/dataview/ApplicationsHistory.svelte
@@ -284,7 +284,7 @@
     remita_confirmation = "checking";
     try {
       const res = await fetch(
-        `${baseURL}/api/files/ManualUpdate?fileId=${fileData.id}&applicationId=${manualUpdate?.id}&userId=${$loggedInUser?.creatorId}&userName=${name}&isCertificate=${isCertificate}`,
+        `${baseURL}/api/files/ManualUpdate?fileId=${fileData.id}&applicationId=${manualUpdate?.id}&userId=${$loggedInUser?.id}&userName=${name}&isCertificate=${isCertificate}`,
         { method: "POST" },
       );
 
