c0bf344ec3305615bb36e432c21aa950b90dfb9b
isaiah415 <okonkwo17@edouniversity.edu.ng>
2026-04-15 13:08:50 +0100

    fix: resolve baseURL definition and clean up commented code

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -6,13 +6,8 @@ import { loggedInUser } from "$lib/store";
 import { goto } from "$app/navigation";
 import type { A } from "vitest/dist/chunks/environment.LoooBwUu.js";
 
-<<<<<<< Updated upstream
- //export const baseURL = "http://localhost:5044";
+// export const baseURL = "http://localhost:5044";
 export const baseURL = "https://backend.einaotest.com";
-=======
-export const baseURL = "http://localhost:5044";
-//  export const baseURL = "https://backend.einaotest.com";
->>>>>>> Stashed changes
 // export const baseURL = "https://integration.iponigeria.com";
 export const localhost = "http://localhost:5044";
 
