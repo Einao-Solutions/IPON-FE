5a101c06f48d1ce14fa4c881e66b90efc3736808
Joel <joel,eriba@einaosolutions.com>
2026-06-10 11:45:53 +0100

    fix: update application status checks for certificate generation and certification documents

--- src/routes/dataview/ApplicationsHistory.svelte
+++ src/routes/dataview/ApplicationsHistory.svelte
@@ -2951,13 +2951,13 @@
                         generateLetter(application, 0, 37);
                       }}>Receipt</DropdownMenu.Item
                     >
-                    {#if application.currentStatus === ApplicationStatuses.Active}
+                    <!-- {#if application.currentStatus === ApplicationStatuses.Active}
                       <DropdownMenu.Item
                         on:click={() => {
                           generateLetter(application, 0, 3);
                         }}>Certificate of Registration</DropdownMenu.Item
                       >
-                    {/if}
+                    {/if} -->
                   {/if}
                   <!-- Appeal Docs -->
                   {#if fileData.type === FileTypes.Trademark && application.applicationType === FormApplicationTypes.AppealRequest && application.currentStatus === ApplicationStatuses.Approved}
@@ -3081,7 +3081,7 @@
                     >
                   {/if}
                   <!-- Certification Docs -->
-                  {#if application.applicationType === FormApplicationTypes.Certification && application.currentStatus !== ApplicationStatuses.AwaitingPayment}
+                  {#if application.applicationType === FormApplicationTypes.Certification && application.currentStatus !== ApplicationStatuses.AwaitingCertification}
                     <DropdownMenu.Item
                       on:click={() => {
                         generateLetter(application, 18, 21);
