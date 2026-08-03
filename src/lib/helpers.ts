4807f6a9016d28b9b9936268e5dc269d37d3b697
Blossom Giane <bloszomgianem@gmail.com>
2026-08-03 11:54:57 +0100

    Offlinerenewal (#101)

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -6,10 +6,10 @@ import { loggedInUser } from "$lib/store";
 import { goto } from "$app/navigation";
 import type { A } from "vitest/dist/chunks/environment.LoooBwUu.js";
 
-//  export const baseURL = "http://localhost:5044";
-export const baseURL = "https://testbackend.iponigeria.com";
+ export const baseURL = "http://localhost:5044";
+// export const baseURL = "https://testbackend.iponigeria.com";
 // export const baseURL = "https://integration.iponigeria.com";
-export const localhost = "http://localhost:5044";
+export const localhost = "http://localhost:5044"   
 
 export const nonConventionalDescription =
   "Filing new patent application with protection in Nigeria only.";
@@ -299,7 +299,10 @@ export enum FormApplicationTypes {
   Restoration = 23,
   CounterStatement = 24,
   StatutoryDeclaration = 25,
+  ChangeOfAgent = 26,
+  OfflineRenewalRequest = 27,
 }
+
 
 export enum ApplicationLetters {
   NewApplicationReceipt = 0,
