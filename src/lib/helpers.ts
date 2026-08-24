2c3065ec17bebb34f5e55d54770b3d67b51dab32
Blossom Giane <bloszomgianem@gmail.com>
2026-08-24 04:07:45 +0100

    removal or trademark late renewal fee a,d remoevof restoration fee, totally

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -7,8 +7,8 @@ import { goto } from "$app/navigation";
 import type { A } from "vitest/dist/chunks/environment.LoooBwUu.js";
 
 // export const baseURL = "http://localhost:5044";
-export const baseURL = "https://backend.einaotest.com";
-// export const baseURL = "https://integration.iponigeria.com";h
+ export const baseURL = "https://testbackend.iponigeria.com";
+// export const baseURL = "https://integration.iponigeria.com";
 export const localhost = "http://localhost:5044"   
 
 export const nonConventionalDescription =
@@ -41,7 +41,7 @@ export type InventorValidator = {
   country: boolean | null;
   city: boolean | null;
   phone: boolean | null;
-  email: boolean | null
+  email: boolean | null;
   address: boolean | null;
 };
 export type DesignCreator = {
