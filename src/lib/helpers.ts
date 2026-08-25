260cef9aa1a228edfdbf40d36c1f590c1521656c
isaiah415 <78854867+isaiah415@users.noreply.github.com>
2026-08-25 06:54:33 +0100

    Super admin/recordals (#113)

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -7,7 +7,7 @@ import { goto } from "$app/navigation";
 import type { A } from "vitest/dist/chunks/environment.LoooBwUu.js";
 
 // export const baseURL = "http://localhost:5044";
- export const baseURL = "https://testbackend.iponigeria.com";
+export const baseURL = "https://backend.einaotest.com";
 // export const baseURL = "https://integration.iponigeria.com";
 export const localhost = "http://localhost:5044"   
 
@@ -41,7 +41,7 @@ export type InventorValidator = {
   country: boolean | null;
   city: boolean | null;
   phone: boolean | null;
-  email: boolean | null;
+  email: boolean | null
   address: boolean | null;
 };
 export type DesignCreator = {
@@ -129,8 +129,6 @@ export const PatentSections = [
     name: "priority",
     description: "Priority Info",
     icon: "pepicons-print:letter",
-  },
-  {
     name: "attachments",
     description: "Attachments",
     icon: "teenyicons:attachment-outline",
